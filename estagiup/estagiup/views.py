77ff24fb60b9c82a205022e0f18ab563946732a3
Dudheloco <145489747+Eduardo-Guimaraes1480@users.noreply.github.com>
2025-07-06 03:08:36 +0000

    feat: Cria a estrutura do Dashboard com sidebar
    - Adiciona a view, URL e template para a página do dashboard.
    - Cria os templates _base_dashboard.html e _sidebar.html para o layout da área logada.
    - Corrige a configuração de arquivos estáticos e importa o CSS.

--- estagiup/estagiup/views.py
+++ estagiup/estagiup/views.py
@@ -2,4 +2,9 @@ from django.shortcuts import render
 
 def index(request):
 
+    context = {}
     return render(request, 'index.html', {'message': 'Bem-vindo ao EstagiUP!'})
+    
+def dashboard_view(request):
+    # No futuro, aqui terá lógica para pegar dados do usuário logado
+    return render(request, 'dashboard.html')
