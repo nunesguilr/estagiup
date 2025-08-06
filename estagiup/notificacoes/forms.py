0bc1bdf9a442846b35d1cd614d66280840df4c57
Yasmin Louren Schultz Carneiro <145464550+yasminschultz@users.noreply.github.com>
2025-08-06 12:47:34 +0000

    Reoganizando

--- estagiup/notificacoes/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Notificacao
-
-class NotificacaoForm(forms.ModelForm):
-    class Meta:
-        model = Notificacao
-        fields = ['mensagem', 'destinatario', 'lido', 'tipo']
