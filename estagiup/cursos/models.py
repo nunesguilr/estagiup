e304d9d9615427373043514139b4be787acd6460
Yasmin Louren Schultz Carneiro <145464550+yasminschultz@users.noreply.github.com>
2025-08-06 13:13:13 +0000

    reestruturando apps

--- estagiup/cursos/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class Curso(models.Model):
-    nome = models.CharField(max_length=255)
-    instituicao = models.ForeignKey('contas.ContaInstituicao', on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.nome
--- estagiup/estagios/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class EstagiosConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "estagios"
