2df71c8353843a2dfe5905ba3e6dced02e9d6a2f
Guilherme Nunes <guermenunes2@gmail.com>
2025-08-12 17:29:09 +0000

    Adição do login e cadastro de usuario

--- estagiup/estagiup/urls.py
+++ estagiup/estagiup/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),   
     path('', views.index, name='home'),
     path('dashboard/', views.dashboard_view, name='dashboard'),
+    path('usuarios/', include('usuario.urls')),
 ]
---
+++ estagiup/usuario/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+app_name = 'usuario'
+
+urlpatterns = [
+    path('registrar/', views.registrar_usuario, name='registrar'),
+    path('login/', views.login_usuario, name='login'),
+    path('lista/', views.lista_usuarios, name='lista_usuarios'),
+]
